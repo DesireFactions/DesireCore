0771b895d0f58214cc7902fa7d1649203c48be83
ctooley21 <christian.tooley@uky.edu>
2017-10-19 17:17:04 -0400

    DESIRE-106

--- src/main/java/com/desiremc/core/tickets/Ticket.java
+++ src/main/java/com/desiremc/core/tickets/Ticket.java
@@ -7,7 +7,8 @@ import org.mongodb.morphia.annotations.Id;
 import org.mongodb.morphia.annotations.Indexed;
 
 @Entity(noClassnameStored = true, value = "tickets")
-public class Ticket {
+public class Ticket
+{
 
     @Id
     private int id;
@@ -32,89 +33,102 @@ public class Ticket {
      * Used exclusively to make a new Ticket. Should never be used on a
      * non-existing ticket.
      *
-     * @param player
-     *            the player who opened the ticket
-     * @param text
-     *            the text of the ticket
+     * @param player the player who opened the ticket
+     * @param text   the text of the ticket
      */
-    public Ticket(UUID player, String text) {
+    public Ticket(UUID player, String text)
+    {
         this.player = player;
         this.opened = System.currentTimeMillis();
         this.text = text;
         this.status = Status.OPEN;
     }
 
-    public Ticket() {
-    }
-
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public UUID getPlayer() {
+    public UUID getPlayer()
+    {
         return player;
     }
 
-    public void setPlayer(UUID player) {
+    public void setPlayer(UUID player)
+    {
         this.player = player;
     }
 
-    public long getOpened() {
+    public long getOpened()
+    {
         return opened;
     }
 
-    public void setOpened(long opened) {
+    public void setOpened(long opened)
+    {
         this.opened = opened;
     }
 
-    public String getText() {
+    public String getText()
+    {
         return text;
     }
 
-    public void setText(String text) {
+    public void setText(String text)
+    {
         this.text = text;
     }
 
-    public UUID getCloser() {
+    public UUID getCloser()
+    {
         return closer;
     }
 
-    public void setCloser(UUID closer) {
+    public void setCloser(UUID closer)
+    {
         this.closer = closer;
     }
 
-    public long getClosed() {
+    public long getClosed()
+    {
         return closed;
     }
 
-    public void setClosed(long closed) {
+    public void setClosed(long closed)
+    {
         this.closed = closed;
     }
 
-    public String getResponse() {
+    public String getResponse()
+    {
         return response;
     }
 
-    public void setResponse(String response) {
+    public void setResponse(String response)
+    {
         this.response = response;
     }
 
-    public Status getStatus() {
+    public Status getStatus()
+    {
         return status;
     }
 
-    public void setStatus(Status status) {
+    public void setStatus(Status status)
+    {
         this.status = status;
     }
 
-    public static enum Status {
+    public static enum Status
+    {
         OPEN,
         CLOSED,
-        DELETED;
+        DELETED
     }
 
 }
