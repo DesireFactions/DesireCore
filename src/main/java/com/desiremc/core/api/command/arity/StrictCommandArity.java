b03e90cac27950d72c7b6f7b92570d606b78f1c3
Michael Ziluck <ziluckm@iastate.edu>
2017-11-11 15:39:51 -0600

    Compile variadic arguments into one string.

--- src/main/java/com/desiremc/core/api/command/arity/StrictCommandArity.java
+++ src/main/java/com/desiremc/core/api/command/arity/StrictCommandArity.java
@@ -1,14 +1,22 @@
 package com.desiremc.core.api.command.arity;
 
+import java.util.List;
+
 public class StrictCommandArity implements CommandArity {
 
     @Override
-    public boolean validateArity(int sentArgsLength, int commandArgsLength) {
-        return sentArgsLength == commandArgsLength;
+    public boolean validateArity(List<String> args, int commandArgsLength) {
+        return args.size() == commandArgsLength;
     }
 
     @Override
     public boolean hasOptional()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isVariadic()
     {
         return false;
     }
