f9279f2e1182adadc5f5d12aa2546dfbbaef36fe
Michael Ziluck <ziluckm@iastate.edu>
2017-11-11 14:19:28 -0600

    Change unban to keep an instance of the active punishment before repeal.

--- src/main/java/com/desiremc/core/commands/punishment/UnbanCommand.java
+++ src/main/java/com/desiremc/core/commands/punishment/UnbanCommand.java
@@ -4,6 +4,7 @@ import com.desiremc.core.DesireCore;
 import com.desiremc.core.api.LangHandler;
 import com.desiremc.core.api.command.ValidCommand;
 import com.desiremc.core.parsers.PlayerSessionParser;
+import com.desiremc.core.punishment.Punishment;
 import com.desiremc.core.punishment.PunishmentHandler;
 import com.desiremc.core.session.Rank;
 import com.desiremc.core.session.Session;
@@ -18,7 +19,7 @@ public class UnbanCommand extends ValidCommand
 
     public UnbanCommand()
     {
-        super("unban", "Unban a user from the server.", Rank.MODERATOR, new String[] {"target"});
+        super("unban", "Unban a user from the server.", Rank.MODERATOR, new String[] { "target" });
         addParser(new PlayerSessionParser(), "target");
 
         addValidator(new PlayerIsBannedValidator(), "target");
@@ -31,8 +32,9 @@ public class UnbanCommand extends ValidCommand
         Session target = (Session) args[0];
 
         LANG.sendRenderMessage(sender, "ban.unban_message", "{player}", target.getName());
-        target.isBanned().setRepealed(true);
-        PunishmentHandler.getInstance().save(target.isBanned());
+        Punishment p = target.isBanned();
+        p.setRepealed(true);
+        PunishmentHandler.getInstance().save(p);
     }
 
 }
