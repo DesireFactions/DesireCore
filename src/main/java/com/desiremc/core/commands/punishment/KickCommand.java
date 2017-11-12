00f9e177d84ce4e2def2e868265cea855619c622
ctooley21 <christian.tooley@uky.edu>
2017-11-12 16:46:36 -0500

    DESIRE-161

--- src/main/java/com/desiremc/core/commands/punishment/KickCommand.java
+++ src/main/java/com/desiremc/core/commands/punishment/KickCommand.java
@@ -1,25 +1,25 @@
 package com.desiremc.core.commands.punishment;
 
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
-
 import com.desiremc.core.DesireCore;
 import com.desiremc.core.api.command.ValidCommand;
 import com.desiremc.core.parsers.PlayerParser;
 import com.desiremc.core.parsers.StringParser;
 import com.desiremc.core.session.Rank;
 import com.desiremc.core.validators.SenderOutranksTargetValidator;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class KickCommand extends ValidCommand
 {
 
     public KickCommand()
     {
-        super("kick", "Kick a user from the server.", Rank.JRMOD, ValidCommand.ARITY_REQUIRED_VARIADIC, new String[] { "target", "reason" });
-        
+        super("kick", "Kick a user from the server.", Rank.JRMOD, ValidCommand.ARITY_REQUIRED_VARIADIC, new String[]
+                {"target", "reason"});
+
         addParser(new PlayerParser(), "target");
         addParser(new StringParser(), "reason");
-        
+
         addValidator(new SenderOutranksTargetValidator(), "target");
     }
 
@@ -29,6 +29,10 @@ public class KickCommand extends ValidCommand
         Player player = (Player) sender;
         Player target = (Player) args[0];
 
-        target.kickPlayer(DesireCore.getLangHandler().renderMessage("staff.kick-message", "{player}", player.getName(), "{reason}", args[1]));
+        target.kickPlayer(DesireCore.getLangHandler().renderMessage("kick.kick_message_target", "{player}", player
+                .getName
+                        (), "{reason}", args[1]));
+        DesireCore.getLangHandler().sendRenderMessage(sender, "kick.kick_message", "{player}", target.getName
+                (), "{reason}", args[1]);
     }
 }
