9a3c4a2c9e092fac63f01e29d0157cb1b06ccaa6
ctooley21 <christian.tooley@uky.edu>
2017-11-17 11:51:08 -0500

    Fix up info command

--- src/main/java/com/desiremc/core/commands/InfoCommand.java
+++ src/main/java/com/desiremc/core/commands/InfoCommand.java
@@ -2,14 +2,13 @@ package com.desiremc.core.commands;
 
 import com.desiremc.core.DesireCore;
 import com.desiremc.core.api.LangHandler;
-import org.bukkit.command.CommandSender;
-
 import com.desiremc.core.api.command.ValidCommand;
 import com.desiremc.core.parsers.PlayerSessionParser;
 import com.desiremc.core.session.Rank;
 import com.desiremc.core.session.Session;
 import com.desiremc.core.utils.DateUtils;
 import com.desiremc.core.validators.PlayerValidator;
+import org.bukkit.command.CommandSender;
 
 /**
  * @author Christian Tooley
@@ -22,7 +21,7 @@ public class InfoCommand extends ValidCommand
 
     public InfoCommand()
     {
-        super("info", "Get information about a player.", Rank.MODERATOR, new String[] { "target" });
+        super("info", "Get information about a player.", Rank.ADMIN, new String[] {"target"});
         addParser(new PlayerSessionParser(), "target");
         addValidator(new PlayerValidator());
     }
@@ -31,26 +30,26 @@ public class InfoCommand extends ValidCommand
     public void validRun(CommandSender sender, String label, Object... args)
     {
         Session target = (Session) args[0];
-        LANG.sendRenderMessage(target, "info.header-footer");
-        LANG.sendCenteredRenderMessage(target.getPlayer(), "info.name", "{name}", target.getName());
-        LANG.sendCenteredRenderMessage(target.getPlayer(), "info.uuid", "{uuid}", target.getUniqueId().toString());
-        LANG.sendCenteredRenderMessage(target.getPlayer(), "info.ip", "{ip}", target.getPlayer().getAddress().getAddress().toString());
-        LANG.sendCenteredRenderMessage(target.getPlayer(), "info.tokens", "{tokens}", target.getTokens() + "");
-        LANG.sendCenteredRenderMessage(target.getPlayer(), "info.status", "{status}", getStatus(target));
-        LANG.sendRenderMessage(target, "info.header-footer");
+        LANG.sendRenderMessageNoPrefix(sender, "info.header-footer");
+        LANG.sendRenderMessageCenteredeNoPrefix(sender, "info.name", "{player}", target.getName());
+        LANG.sendRenderMessageCenteredeNoPrefix(sender, "info.uuid", "{uuid}", target.getUniqueId().toString());
+        LANG.sendRenderMessageCenteredeNoPrefix(sender, "info.ip", "{ip}", target.getIp());
+        LANG.sendRenderMessageCenteredeNoPrefix(sender, "info.tokens", "{tokens}", target.getTokens() + "");
+        LANG.sendRenderMessageCenteredeNoPrefix(sender, "info.status", "{status}", getStatus(target));
+        LANG.sendRenderMessageNoPrefix(sender, "info.header-footer");
     }
 
     private String getStatus(Session player)
     {
         if (player.isBanned() != null)
         {
-            return LANG.renderMessage("info.banned", "{time}", longToTime(player.isBanned().getExpirationTime()));
+            return LANG.renderMessageNoPrefix("info.banned", "{time}", longToTime(player.isBanned().getExpirationTime()));
         }
         if (player.isMuted() != null)
         {
-            return LANG.renderMessage("info.muted", "{time}", longToTime(player.isBanned().getExpirationTime()));
+            return LANG.renderMessageNoPrefix("info.muted", "{time}", longToTime(player.isBanned().getExpirationTime()));
         }
-        return LANG.renderMessage("info.normal");
+        return LANG.renderMessageNoPrefix("info.normal");
     }
 
     private String longToTime(long time)
