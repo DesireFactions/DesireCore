ef41bc56dbccdb128fdcdf003c3fe4fc871c09b5
Michael Ziluck <ziluckm@iastate.edu>
2017-11-20 15:11:53 -0600

    Code cleaning.

--- src/main/java/com/desiremc/core/validators/ItemInHandValidator.java
+++ src/main/java/com/desiremc/core/validators/ItemInHandValidator.java
@@ -5,22 +5,25 @@ import org.bukkit.entity.Player;
 
 import com.desiremc.core.DesireCore;
 
-public class ItemInHandValidator extends PlayerValidator {
+public class ItemInHandValidator extends PlayerValidator
+{
 
     @Override
-    public boolean validateArgument(CommandSender sender, String label, Object arg) {
-        boolean first = super.validateArgument(sender, label, arg);
-        if (!first) {
+    public boolean validateArgument(CommandSender sender, String label, Object arg)
+    {
+        if (!super.validateArgument(sender, label, arg))
+        {
             return false;
         }
-        
+
         Player p = (Player) sender;
-        if (p.getItemInHand() == null) {
+        if (p.getItemInHand() == null)
+        {
             DesireCore.getLangHandler().sendString(sender, "item_in_hand");
             return false;
         }
-        
+
         return true;
     }
-    
+
 }
