ce7e9ed90f7aaef1595ff42471d1a7bb5175d29d
Michael Ziluck <ziluckm@iastate.edu>
2017-11-22 01:31:14 -0500

    Test new tablist.

---
+++ src/main/java/com/desiremc/core/oldtablist/Entry.java
@@ -0,0 +1,245 @@
+package com.desiremc.core.oldtablist;
+
+import java.util.UUID;
+
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_7_R4.CraftWorld;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Team;
+
+import com.desiremc.core.utils.ReflectionUtils;
+import com.desiremc.core.utils.ReflectionUtils.NMSFields;
+
+import net.minecraft.server.v1_7_R4.EntityPlayer;
+import net.minecraft.server.v1_7_R4.MinecraftServer;
+import net.minecraft.server.v1_7_R4.PacketPlayOutPlayerInfo;
+import net.minecraft.server.v1_7_R4.PlayerConnection;
+import net.minecraft.server.v1_7_R4.PlayerInteractManager;
+import net.minecraft.server.v1_7_R4.TileEntitySkull;
+import net.minecraft.server.v1_7_R4.WorldServer;
+import net.minecraft.util.com.mojang.authlib.GameProfile;
+
+public class Entry
+{
+
+    private Tab playerTab;
+
+    private int x;
+    private int y;
+    private String text;
+    private String skin;
+
+    private EntityPlayer nms;
+    private UUID uuid;
+
+    private Team team;
+    private boolean setup;
+
+    public Entry(Tab playerTab, String text, int x, int y)
+    {
+        this(playerTab, text, x, y, null);
+    }
+
+    public Entry(Tab playerTab, String text, int x, int y, String skin)
+    {
+        this(playerTab, text, x, y, skin, UUID.randomUUID());
+    }
+
+    public Entry(Tab playerTab, String text, int x, int y, String skin, UUID uuid)
+    {
+        this.playerTab = playerTab;
+        this.text = text;
+        this.x = x;
+        this.y = y;
+        this.skin = skin;
+        this.uuid = uuid;
+        playerTab.addEntry(this);
+    }
+
+    private Entry setup()
+    {
+        this.setup = true;
+        Player player = this.playerTab.getPlayer();
+        CraftPlayer craftplayer = (CraftPlayer) player;
+
+        if (text == null)
+        {
+            text = "";
+        }
+        GameProfile profile = new GameProfile((uuid = UUID.randomUUID()), ChatColor.translateAlternateColorCodes('&', this.text));
+        if (skin != null)
+        {
+            GameProfile skinProfile = TileEntitySkull.skinCache.getUnchecked(skin.toLowerCase());
+            ReflectionUtils.setValue(skinProfile, NMSFields.gameProfileId, uuid);
+            ReflectionUtils.setValue(skinProfile, NMSFields.gameProfileId, text);
+            profile = skinProfile;
+        }
+        this.nms = new EntityPlayer(MinecraftServer.getServer(), ((CraftWorld) player.getWorld()).getHandle(), profile, new PlayerInteractManager(((CraftWorld) player.getWorld()).getHandle()));
+        PacketPlayOutPlayerInfo packet = PacketPlayOutPlayerInfo.updateDisplayName(this.nms);
+
+        craftplayer.getHandle().playerConnection.sendPacket(packet);
+        this.team = this.playerTab.getScoreboard().registerNewTeam(uuid.toString().substring(0, 16));
+        this.team.addEntry(this.nms.getName());
+        return this;
+    }
+
+    public Entry send()
+    {
+        if (!this.setup)
+        {
+            return this.setup();
+        }
+        else
+        {
+            // translate the text color
+            this.text = ChatColor.translateAlternateColorCodes('&', this.text);
+
+            if (this.text.length() > 16)
+            {
+                this.team.setPrefix(this.text.substring(0, 16));
+                String suffix = ChatColor.getLastColors(this.team.getPrefix()) + this.text.substring(16, this.text.length());
+
+                if (suffix.length() > 16)
+                {
+                    if (suffix.length() <= 16)
+                    {
+                        suffix = this.text.substring(16, this.text.length());
+                        this.team.setSuffix(suffix.substring(0, suffix.length()));
+                    }
+                    else
+                    {
+                        this.team.setSuffix(suffix.substring(0, 16));
+                    }
+                }
+                else
+                {
+                    this.team.setSuffix(suffix);
+                }
+            }
+            else
+            {
+                this.team.setPrefix(this.text);
+                this.team.setSuffix("");
+            }
+
+            return this;
+        }
+    }
+
+    public Tab getPlayerTab()
+    {
+        return this.playerTab;
+    }
+
+    public Entry setPlayerTab(Tab playerTab)
+    {
+        this.playerTab = playerTab;
+        return this;
+    }
+
+    public String getText()
+    {
+        return this.text;
+    }
+
+    public Entry setText(String text)
+    {
+        this.text = text;
+        return this;
+    }
+
+    public String getSkin()
+    {
+        return skin;
+    }
+
+    public Entry setSkin(String skin)
+    {
+        this.skin = skin;
+        GameProfile profile = nms.getProfile();
+        ReflectionUtils.setValue(profile, NMSFields.gameProfileId, uuid);
+        ReflectionUtils.setValue(profile, NMSFields.gameProfileName, text);
+        ((CraftPlayer) playerTab.getPlayer()).getHandle().playerConnection.sendPacket(PacketPlayOutPlayerInfo.updateDisplayName(nms));
+        return this;
+    }
+
+    public UUID getUniqueId()
+    {
+        return uuid;
+    }
+
+    public Entry setUniqueId(UUID uuid)
+    {
+        PlayerConnection pc = ((CraftPlayer) playerTab.getPlayer()).getHandle().playerConnection;
+        pc.sendPacket(PacketPlayOutPlayerInfo.removePlayer(nms));
+
+        this.uuid = uuid;
+        GameProfile profile = new GameProfile(uuid, text);
+        if (skin != null)
+        {
+            GameProfile skinProfile = TileEntitySkull.skinCache.getUnchecked(skin.toLowerCase());
+            ReflectionUtils.setValue(skinProfile, NMSFields.gameProfileId, uuid);
+            ReflectionUtils.setValue(skinProfile, NMSFields.gameProfileId, text);
+            profile = skinProfile;
+        }
+        this.nms = new EntityPlayer(MinecraftServer.getServer(), (WorldServer) nms.world, profile, new PlayerInteractManager(((CraftWorld) playerTab.getPlayer().getWorld()).getHandle()));
+
+        PacketPlayOutPlayerInfo packet = PacketPlayOutPlayerInfo.updateDisplayName(this.nms);
+
+        pc.sendPacket(packet);
+        this.team = this.playerTab.getScoreboard().registerNewTeam(uuid.toString().substring(0, 16));
+        this.team.addEntry(this.nms.getName());
+        return this;
+    }
+
+    public Team getTeam()
+    {
+        return this.team;
+    }
+
+    public Entry setTeam(Team team)
+    {
+        this.team = team;
+        return this;
+    }
+
+    public int getX()
+    {
+        return this.x;
+    }
+
+    public Entry setX(int x)
+    {
+        this.x = x;
+        return this;
+    }
+
+    public int getY()
+    {
+        return this.y;
+    }
+
+    public Entry setY(int y)
+    {
+        this.y = y;
+        return this;
+    }
+
+    public EntityPlayer getNms()
+    {
+        return this.nms;
+    }
+
+    public Entry setNms(EntityPlayer nms)
+    {
+        this.nms = nms;
+        return this;
+    }
+
+    public Entry setup(boolean setup)
+    {
+        this.setup = setup;
+        return this;
+    }
+}
---
+++ src/main/java/com/desiremc/core/oldtablist/Tab.java
@@ -0,0 +1,210 @@
+package com.desiremc.core.oldtablist;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.Scoreboard;
+
+import com.desiremc.core.oldtablist.events.TabCreateEvent;
+
+import net.minecraft.server.v1_7_R4.PacketPlayOutPlayerInfo;
+
+public class Tab
+{
+
+    private static final boolean DEBUG = true;
+
+    private static ConcurrentHashMap<Tab, Boolean> playerTabs = new ConcurrentHashMap<>();
+    private Player player;
+    private Scoreboard scoreboard;
+    private HashMap<UUID, Entry> entries;
+
+    public Tab(final Player player)
+    {
+        this.player = player;
+        this.entries = new HashMap<>();
+        this.clear();
+        if (!player.getScoreboard().equals(Bukkit.getScoreboardManager().getMainScoreboard()))
+        {
+            this.scoreboard = player.getScoreboard();
+            this.assemble();
+        }
+        else
+        {
+            this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
+            player.setScoreboard(this.scoreboard);
+            this.assemble();
+        }
+
+        Tab.playerTabs.put(this, true);
+    }
+
+    public void clear()
+    {
+        Iterator<Entry> iterator = this.entries.values().iterator();
+
+        while (iterator.hasNext())
+        {
+            Entry online = iterator.next();
+
+            if (online.getNms() != null)
+            {
+                PacketPlayOutPlayerInfo packet = PacketPlayOutPlayerInfo.removePlayer(online.getNms());
+
+                ((CraftPlayer) this.player).getHandle().playerConnection.sendPacket(packet);
+            }
+        }
+
+        for (Player player : Bukkit.getOnlinePlayers())
+        {
+            PacketPlayOutPlayerInfo packet1 = PacketPlayOutPlayerInfo.removePlayer(((CraftPlayer) player).getHandle());
+            ((CraftPlayer) this.player).getHandle().playerConnection.sendPacket(packet1);
+        }
+
+        this.entries.clear();
+    }
+
+    public void assemble()
+    {
+        for (int i = 0; i < 60; ++i)
+        {
+            int x = i % 3;
+            int y = i / 3;
+
+            new Entry(this, this.getNextBlank(), x, y).send();
+        }
+
+        Bukkit.getPluginManager().callEvent(new TabCreateEvent(this));
+    }
+
+    public Entry getEntry(int x, int y)
+    {
+        Iterator<Entry> iterator = this.entries.values().iterator();
+
+        Entry tabEntry;
+
+        do
+        {
+            if (!iterator.hasNext())
+            {
+                return null;
+            }
+
+            tabEntry = iterator.next();
+        } while (tabEntry.getX() != x || tabEntry.getY() != y);
+
+        return tabEntry;
+    }
+
+    public String getNextBlank()
+    {
+        Iterator<String> iterator = getAllBlanks().iterator();
+
+        label23: while (iterator.hasNext())
+        {
+            String string = iterator.next();
+            Iterator<Entry> iterator1 = this.entries.values().iterator();
+
+            while (iterator1.hasNext())
+            {
+                Entry tabEntry = iterator1.next();
+
+                if (tabEntry.getText() != null && tabEntry.getText().startsWith(string))
+                {
+                    continue label23;
+                }
+            }
+
+            return string;
+        }
+        if (DEBUG)
+        {
+            System.out.println("Tab.getNextBlank() returned null.");
+        }
+        return null;
+    }
+
+    private static List<String> getAllBlanks()
+    {
+        ArrayList<String> toReturn = new ArrayList<>();
+        ChatColor[] achatcolor;
+        int i = (achatcolor = ChatColor.values()).length;
+
+        for (int j = 0; j < i; ++j)
+        {
+            ChatColor chatColor = achatcolor[j];
+
+            toReturn.add("" + chatColor + ChatColor.RESET);
+            ChatColor[] achatcolor1;
+            int k = (achatcolor1 = ChatColor.values()).length;
+
+            for (int l = 0; l < k; ++l)
+            {
+                ChatColor chatColor1 = achatcolor1[l];
+
+                if (toReturn.size() >= 60)
+                {
+                    return toReturn;
+                }
+
+                toReturn.add("" + chatColor + chatColor1 + ChatColor.RESET);
+            }
+        }
+
+        return toReturn;
+    }
+
+    public static Tab getByPlayer(Player player)
+    {
+        for (Tab t : playerTabs.keySet())
+        {
+            if (t.getPlayer().getName().equals(player.getName()))
+            {
+                return t;
+            }
+        }
+        return null;
+    }
+
+    public static Set<Tab> getPlayerTabs()
+    {
+        return Tab.playerTabs.keySet();
+    }
+
+    public Player getPlayer()
+    {
+        return this.player;
+    }
+
+    public Scoreboard getScoreboard()
+    {
+        return this.scoreboard;
+    }
+
+    public void addEntry(Entry entry)
+    {
+        if (entry.getUniqueId() == null)
+        {
+            throw new IllegalArgumentException("Entry has null UUID.");
+        }
+        this.entries.put(entry.getUniqueId(), entry);
+    }
+
+    public void removeEntry(UUID uuid)
+    {
+        Entry entry = this.entries.remove(uuid);
+        if (entry != null)
+        {
+            entry.setText("").send();
+        }
+    }
+}
