fa4803642d0d5c03f6c728c3e19c66516bca96c8
Michael Ziluck <ziluckm@iastate.edu>
2017-11-28 09:31:52 -0600

    Add item stack serialization methods.

--- src/main/java/com/desiremc/core/utils/ItemUtils.java
+++ src/main/java/com/desiremc/core/utils/ItemUtils.java
@@ -1,11 +1,15 @@
 package com.desiremc.core.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Field;
 
 import org.bukkit.craftbukkit.v1_7_R4.inventory.CraftItemStack;
 import org.bukkit.inventory.ItemStack;
 
+import net.minecraft.server.v1_7_R4.NBTCompressedStreamTools;
 import net.minecraft.server.v1_7_R4.NBTTagCompound;
+import net.minecraft.util.com.google.common.io.BaseEncoding;
 
 public class ItemUtils
 {
@@ -14,6 +18,25 @@ public class ItemUtils
     public static CraftItemStack getCraftItemStack(ItemStack item)
     {
         return item instanceof CraftItemStack ? (CraftItemStack) item : CraftItemStack.asCraftCopy(item);
+    }
+
+    public static String serializeItem(ItemStack item)
+    {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        NBTTagCompound tag = getTag(getHandle(getCraftItemStack(item)));
+        NBTCompressedStreamTools.a(tag, output);
+
+        return BaseEncoding.base64().encode(output.toByteArray());
+    }
+
+    public static ItemStack deserializeItem(String str)
+    {
+        ByteArrayInputStream input = new ByteArrayInputStream(BaseEncoding.base64().decode(str));
+
+        NBTTagCompound tag = NBTCompressedStreamTools.a(input);
+        net.minecraft.server.v1_7_R4.ItemStack nms = net.minecraft.server.v1_7_R4.ItemStack.createStack(tag);
+
+        return CraftItemStack.asBukkitCopy(nms);
     }
 
     public static net.minecraft.server.v1_7_R4.ItemStack getHandle(CraftItemStack item)
