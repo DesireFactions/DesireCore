a2a57f634436a3cc69dfb55195dedaddd4cfd13f
Michael Ziluck <ziluckm@iastate.edu>
2017-12-06 18:18:23 -0600

    Update formatting.

--- src/main/java/com/desiremc/core/commands/InfoCommand.java
+++ src/main/java/com/desiremc/core/commands/InfoCommand.java
@@ -3,14 +3,23 @@ package com.desiremc.core.commands;
 import java.util.List;
 
 import com.desiremc.core.DesireCore;
+import com.desiremc.core.api.LangHandler;
 import com.desiremc.core.api.newcommands.CommandArgument;
 import com.desiremc.core.api.newcommands.CommandArgumentBuilder;
 import com.desiremc.core.api.newcommands.ValidCommand;
 import com.desiremc.core.newparsers.SessionParser;
+import com.desiremc.core.punishment.Punishment;
+import com.desiremc.core.punishment.Punishment.Type;
 import com.desiremc.core.session.Rank;
 import com.desiremc.core.session.Session;
 import com.desiremc.core.utils.DateUtils;
 
+/**
+ * Command used to display the player's basic information values.
+ * 
+ * @author Michael Ziluck and Christian Tooley
+ * @since 12/06/2017
+ */
 public class InfoCommand extends ValidCommand
 {
     public InfoCommand()
@@ -32,17 +41,71 @@ public class InfoCommand extends ValidCommand
         DesireCore.getLangHandler().sendRenderMessageNoPrefix(sender, "info.header-footer");
     }
 
+    /**
+     * Gets the player's punishment status.
+     * 
+     * @param player the player to check.
+     * @return the string representation of their punishment status.
+     */
     private String getStatus(Session player)
     {
-        if (player.isBanned() != null)
+        LangHandler lang = DesireCore.getLangHandler();
+
+        // check if they are ip banned
+        Punishment punishment = player.isIPBanned();
+
+        // check if they are banned normally
+        if (punishment == null)
         {
-            return DesireCore.getLangHandler().renderMessageNoPrefix("info.banned", "{time}", longToTime(player.isBanned().getExpirationTime()));
+            punishment = player.isBanned();
         }
-        if (player.isMuted() != null)
+
+        // check if they are muted
+        if (punishment == null)
         {
-            return DesireCore.getLangHandler().renderMessageNoPrefix("info.muted", "{time}", longToTime(player.isBanned().getExpirationTime()));
+            punishment = player.isMuted();
         }
-        return DesireCore.getLangHandler().renderMessageNoPrefix("info.normal");
+
+        // if they this is still null, they have a normal status
+        if (punishment == null)
+        {
+            return lang.renderMessageNoPrefix("info.normal");
+        }
+
+        // return ip ban message, it never has a time
+        if (punishment.getType() == Type.IP_BAN)
+        {
+            return lang.renderMessageNoPrefix("info.ipbanned");
+        }
+
+        // return the ban message, varies by permanence
+        if (punishment.getType() == Type.BAN)
+        {
+            if (punishment.isPermanent())
+            {
+                return lang.renderMessageNoPrefix("info.permbanned");
+            }
+            else
+            {
+                return lang.renderMessageNoPrefix("info.banned", "{time}", longToTime(punishment.getExpirationTime()));
+            }
+        }
+
+        // return the mute message, varies by permanence
+        if (punishment.getType() == Type.MUTE)
+        {
+            if (punishment.isPermanent())
+            {
+                return lang.renderMessageNoPrefix("info.permmuted");
+            }
+            else
+            {
+                return lang.renderMessageNoPrefix("info.muted", "{time}", longToTime(punishment.getExpirationTime()));
+            }
+        }
+
+        // this shouldn't be possible as we terminated earlier
+        return null;
     }
 
     private String longToTime(long time)
