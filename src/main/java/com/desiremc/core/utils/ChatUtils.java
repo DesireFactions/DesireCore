ffb6d24244fd2ab2e3814c8b929634ef956c31b8
ctooley21 <christian.tooley@uky.edu>
2017-12-25 19:54:07 -0600

    fixing

--- src/main/java/com/desiremc/core/utils/ChatUtils.java
+++ src/main/java/com/desiremc/core/utils/ChatUtils.java
@@ -34,47 +34,7 @@ public class ChatUtils
 
     private static void sendCenteredMessageFinal(Session session, String message)
     {
-        Player sender = session.getPlayer();
-        if (message == null || message.equals(""))
-        {
-            sender.sendMessage("");
-        }
-        message = ChatColor.translateAlternateColorCodes('&', message);
-
-        int messagePxSize = 0;
-        boolean previousCode = false;
-        boolean isBold = false;
-
-        for (char c : message.toCharArray())
-        {
-            if (c == ChatColor.COLOR_CHAR)
-            {
-                previousCode = true;
-            }
-            else if (previousCode)
-            {
-                previousCode = false;
-                isBold = (c == 'l' || c == 'L');
-            }
-            else
-            {
-                DefaultFontInfo dFI = DefaultFontInfo.getDefaultFontInfo(c);
-                messagePxSize += isBold ? dFI.getBoldLength() : dFI.getLength();
-                messagePxSize++;
-            }
-        }
-
-        int halvedMessageSize = messagePxSize / 2;
-        int toCompensate = CENTER_PX - halvedMessageSize;
-        int spaceLength = DefaultFontInfo.SPACE.getLength() + 1;
-        int compensated = 0;
-        StringBuilder sb = new StringBuilder();
-        while (compensated < toCompensate)
-        {
-            sb.append(" ");
-            compensated += spaceLength;
-        }
-        sender.sendMessage(sb.toString() + message);
+        session.sendMessage(renderCenteredMessage(message));
     }
 
     public static String renderCenteredMessage(String message)
