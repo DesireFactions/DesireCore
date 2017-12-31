20a38b672f1d3c806e7613e238a339e9b9477f22
ctooley21 <christian.tooley@uky.edu>
2017-12-30 21:35:28 -0600

    Make sure we account for the fact that TimeParser adds System time

--- src/main/java/com/desiremc/core/validators/NumberSizeValidator.java
+++ src/main/java/com/desiremc/core/validators/NumberSizeValidator.java
@@ -68,7 +68,7 @@ public class NumberSizeValidator<T extends Number> implements Validator<T>
     @Override
     public boolean validateArgument(Session sender, String[] label, T arg)
     {
-        double val = arg.doubleValue();
+        double val = arg.doubleValue() - System.currentTimeMillis();
 
         System.out.println("Entered: " + val + " Low end: " + low.doubleValue() + " High end: " + high.doubleValue());
 
