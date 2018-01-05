05d11fa31b19346c5044951354dc14713d93d3b0
ctooley21 <christian.tooley@uky.edu>
2018-01-04 18:09:07 -0600

    Remove default rank for chat commands

--- src/main/java/com/desiremc/core/commands/chat/ChatCommand.java
+++ src/main/java/com/desiremc/core/commands/chat/ChatCommand.java
@@ -1,13 +1,12 @@
 package com.desiremc.core.commands.chat;
 
 import com.desiremc.core.api.newcommands.ValidBaseCommand;
-import com.desiremc.core.session.Rank;
 
 public class ChatCommand extends ValidBaseCommand
 {
     public ChatCommand()
     {
-        super("chat", "staff chat tools", Rank.ADMIN);
+        super("chat", "staff chat tools");
         addSubCommand(new ChatClearCommand());
         addSubCommand(new ChatToggleCommand());
         addSubCommand(new ChatSlowCommand());
